74c0a4215ea8d5e2b0ffd512832882c8639f7cda
Rafaela <rafaela.fae@gmail.com>
2025-07-28 13:33:59 -0300

    Desenvolvimento de Validação ded check e seu respesctivo teste

--- src/use-cases/validate-check-in.spec.ts
+++ src/use-cases/validate-check-in.spec.ts
@@ -2,6 +2,7 @@ import { InMemoryCheckInsRepository } from '@/repositories/in-memory/in-memory-c
 import { expect, describe, it, beforeEach, vi, afterEach } from 'vitest'
 import { ValidateCheckInUseCase } from './validate-check-in'
 import { ResourceNotFoundError } from './errors/resource-not-found-error'
+import { LateCheckInValidationError } from './errors/late-check-in-validation'
 
 let checkInsRepository: InMemoryCheckInsRepository
 let sut: ValidateCheckInUseCase
@@ -11,14 +12,15 @@ describe('Validate Check In Use Case', () => {
         checkInsRepository = new InMemoryCheckInsRepository()
         sut = new ValidateCheckInUseCase(checkInsRepository)
 
-        // vi.useFakeTimers()
+        vi.useFakeTimers()
     })
 
     afterEach(() => {
-        //vi.useRealTimers()
+        vi.useRealTimers()
     })
 
     it('should be able to validate check-in', async () => {
+
         const createdCheckIn = await checkInsRepository.create(
             {
                 user_id: 'user-01',
@@ -43,5 +45,26 @@ describe('Validate Check In Use Case', () => {
                 checkInId: 'inexistent-check-in',
             }),
         ).rejects.toBeInstanceOf(ResourceNotFoundError)
+    })
+
+    it('should not be able to validate the check-in after 20 minutes of its creation', async () => {
+        vi.setSystemTime(new Date(2024, 0, 1, 13, 40))
+
+        const createdCheckIn = await checkInsRepository.create(
+            {
+                user_id: 'user-01',
+                gym_id: 'gym-01',
+            }
+        )
+
+        const twentyOneMinutes = 1000 * 60 * 21
+
+        vi.advanceTimersByTime(twentyOneMinutes)
+
+        await expect(() =>
+            sut.execute({
+                checkInId: createdCheckIn.id,
+            })
+        ).rejects.toBeInstanceOf(LateCheckInValidationError)
     })
 })
